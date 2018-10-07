016fb430042760e2c05b02edecef093506f3b56b
mistval <randallschmidt1@gmail.com>
2018-10-07 14:20:36 +0800

    improve use of bootstrap by having all row children be columns

--- react-frontend/src/bot/bot.jsx
+++ react-frontend/src/bot/bot.jsx
@@ -43,7 +43,7 @@ function createExamplesJsx(examples) {
 function createCommandsJsx() {
   return commands.map((command) => {
     return (
-      <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12 pl-0 pr-4 pb-4" key={command.key}>
+      <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12 pr-2 pb-4" key={command.key}>
         <div className="card">
           <div className="card-body">
             <h5 className="card-title">{command.primaryCommand}</h5>
@@ -67,12 +67,12 @@ function createCommandsJsx() {
 function Commands() {
   return (
     <div>
-      <div className="row pl-5 pb-4 pt-5">
-        <div className="col-sm-12 pl-0">
+      <div class="row pl-5 pt-5">
+        <div class="col-sm-12">
           <h3>Commands</h3>
         </div>
       </div>
-      <div className="row pl-5 pr-5">
+      <div className="row pt-3 pl-5 pr-5">
         {createCommandsJsx()}
       </div>
     </div>
@@ -82,18 +82,20 @@ function Commands() {
 function Header() {
   return (
     <div className="row p-5">
-      <img alt="bot avatar" id="avatar" src={Avatar} />
-      <div className="pl-3">
-        <div className="pl-3 mb-1">
-          <h5>Kotoba Discord Bot</h5>
+      <div className="col-sm-12">
+        <img class="align-top" alt="bot avatar" id="avatar" src={Avatar} />
+        <div className="inline-block ml-4">
+          <h5 className="mb-2">Kotoba Discord Bot</h5>
           <a href="https://discordbots.org/bot/251239170058616833"><img src="https://discordbots.org/api/widget/status/251239170058616833.svg" alt="Discord Bots" /></a>
           &nbsp;
           <a href="https://discordbots.org/bot/251239170058616833"><img src="https://discordbots.org/api/widget/servers/251239170058616833.svg" alt="Discord Bots" /></a>
           <br />
+          <div className="mt-3">
+            <a href="https://discordapp.com/oauth2/authorize?client_id=251239170058616833&scope=bot" target="_blank">DISCORD INVITE</a>
+            <a className="ml-4" href="https://github.com/mistval/kotoba" target="_blank">GITHUB</a>
+            <a className="ml-4" href="https://discord.gg/zkAKbyJ" target="_blank">HELP SERVER</a>
+          </div>
         </div>
-        <a className="btn btn-primary" href="https://discordapp.com/oauth2/authorize?client_id=251239170058616833&scope=bot">Discord Invite</a>
-        <a className="btn btn-primary" href="https://github.com/mistval/kotoba">Github</a>
-        <a className="btn btn-primary" href="https://discord.gg/zkAKbyJ">Help</a>
       </div>
     </div>
   );
